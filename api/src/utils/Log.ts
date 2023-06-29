7203513cc89827f023167adcb1191c4c8944f4be
Daniel Yukio <danielyukio12@gmail.com>
2023-06-28 22:48:07 -0300

    revert deletion

---
+++ api/src/utils/Log.ts
@@ -0,0 +1,42 @@
+export function Log(description: string, log: any): { description: string, log: string } {
+
+    const title = '[ATM]';
+    const datetime = `[${new Date().toLocaleString()}]`;
+
+    let message = '';
+
+    switch (typeof log) {
+        case 'string':
+            message += log;
+            break;
+        case 'object':
+            if (log.error && typeof log.error === 'string') {
+                message += log.error;
+            } else if (log.message && typeof log.message === 'string') {
+                message += log.message;
+            } else {
+                message += JSON.stringify(log);
+            }
+            break;
+        default:
+            message += JSON.stringify(log);
+            break;
+    }
+
+    if (message && description) {
+
+        console.log(`${title}-${datetime}: ${description} - ${message}`);
+        return { description, log: message };
+
+    } else if (!message && description) {
+
+        console.log(`${title}-${datetime}: ${description}`);
+        return { description, log: '' };
+
+    } else {
+
+        console.log(`${title}-${datetime}: ${message}`);
+        return { description, log: message };
+
+    }
+}
--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,2 +1,3 @@
+export * from './Log';
 export * from './HttpResponse';
 export * from './SinglePrisma';
